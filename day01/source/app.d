88d046c723aab39069bfefbfa3fcc623b7028e9d
Fireeye <fireeye64@gmail.com>
2022-12-02 21:50:46 +0100

    Refactored day 1 into a single project.

--- day01/source/app.d
+++ day01/source/app.d
@@ -3,6 +3,8 @@ import std.string : strip;
 import std.range: empty;
 import std.algorithm.searching: maxElement;
 import std.conv : to;
+import std.algorithm.sorting: sort;
+import std.algorithm.iteration: sum;
 
 long[] caloriesTotalList;
 
@@ -18,7 +20,11 @@ void main() {
 			tmpCalories += to!long(line.strip());
 		}
 	}
+	caloriesTotalList.sort!("a > b");
 	if (caloriesTotalList.length > 0) {
-		writeln(maxElement(caloriesTotalList));
+		writeln("Highest calorie count: ", caloriesTotalList[0]);
+	}
+	if (caloriesTotalList.length > 3) {
+		writeln("Total of top 3 elements: ", sum(caloriesTotalList[0..3]));
 	}
 }
